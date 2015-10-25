46b16c50846af0d244881b3f5737b296e81fd56a
ΣΠΥΡΙΔΩΝ <@sasaria>
2015-10-25 19:25:10 +0200

    testt

---
+++ src/test/Test.java
@@ -0,0 +1,17 @@
+
+package test;
+
+/**
+ *
+ * @author ΣΠΥΡΙΔΩΝ
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
